f94e55a8df12b6462696a07f6df17e1a4cb15440
sohelkh1211 <khansohailabdussalam207@gmail.com>
2024-06-25 00:28:07 +0530

    Preloader fixing.

--- src/components/Footer1.jsx
+++ src/components/Footer1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer1 = () => {
   return (
-    <footer className='absolute left-0 mt-[100px] flex items-center justify-center w-screen h-[40px] bg-purple-500'>
+    <footer className='absolute left-0 xs:mt-[205px] sm:mt-[100px] flex items-center justify-center w-screen h-[40px] bg-purple-500'>
         <h1 className='font-bold mx-auto text-[19px]'>Made with ❤️ by <a className='font-bold italic underline cursor-pointer' href='https://www.linkedin.com/in/mohammed-sohel-khan-452479259/' target='_ blank' rel="noopener">Sohel Khan</a></h1>
     </footer>
   )
