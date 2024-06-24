aeec824aa12d2fffe787013307dbd9ddd3a71aca
sohelkh1211 <khansohailabdussalam207@gmail.com>
2024-06-25 00:06:20 +0530

    Preloader fixing.

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -4,7 +4,7 @@ const Preloader = () => {
     return (
         <div className="fixed top-0 left-0">
             <div className="relative z-1">
-                <img src="https://cdn.svgator.com/images/2023/06/hearthbeat-svg-loader-animation.svg" alt="Heart Monitor SVG loader animation" loading="lazy" className='lg:h-[600px] md:h-[500px] sm:w-screen xs:object-cover xs:h-[500px]' />
+                <img src="https://cdn.svgator.com/images/2023/06/hearthbeat-svg-loader-animation.svg" alt="Heart Monitor SVG loader animation" loading="lazy" className='lg:h-[600px] md:h-[500px] sm:w-screen xs:w-screen xs:h-[500px]' />
             </div>
             <div className='absolute flex xs:top-[150px] lg:ml-[44%] md:ml-[44%] sm:ml-[43%] xs:ml-[37%] border-none border-black'>
                 <p className="text-white lg:text-[36px] xs:text-[25px]">Healthify</p>
